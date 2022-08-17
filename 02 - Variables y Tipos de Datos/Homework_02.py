505e3b8471191737c68e276f10f9a5741459baa9
Stardust1574 <ariostobandala@gmail.com>
2022-08-17 13:51:54 -0500

    Tarea y notas

---
+++ 02 - Variables y Tipos de Datos/Homework_02.py
@@ -0,0 +1,74 @@
+from cmath import pi
+
+
+Variable1 = 1243
+print(Variable1)
+
+type (8.5)
+
+type(Variable1)
+
+Nombre = "Ariosto B"
+
+Varcomplex = 12 * 5j
+
+type(Varcomplex)
+
+pi = 3.1416
+
+V1 = "True"
+V2 = True
+
+type(V1)
+type(V2)
+
+V3 = 10 + 0.74937
+
+h= 3.14
+V4 = 10 + 5(h)
+print(V4)
+
+c = h + 5.45
+print(c)
+
+print(5 * 4)
+
+print(2**8) 
+
+n_cociente = 27 / 4
+print(n_cociente)
+
+print(27//4)
+
+print(27 % 4)
+
+print (6 * 4 + 3)
+
+a1 = "tu puntaje recibido fue "
+a2 = "9.20"
+print(a1 + a2)
+
+2 == "2"
+
+2 ==int ("2")
+
+a = float('3,8') 
+# ES UN STRING, POR LO TANTO NO PUEDE CONVERTIR EL NUMERO A UN DATO FLOTANTE
+
+x = 3
+x -= 4 
+print (x)
+
+1 << 2
+#ESTO OCURRE EN EL SISTEMA DE NUMERACIÓN BINARIO PORQUE SOLO ESTAN REPRESENTADOS LOS NUMEROS 0 y 1
+
+2 + "2"
+# ESTA OPERACION NO SE PUEDE REALIZAR POR QUE LOS DATOS NO SON DEL MISMO TIPO
+# SI LOS DATOS FUERAN DEL MISMO TIPO TENDRIAMOS DOS RESULTADOS DIFERENTES DEPENDIENDO EL TIPO,
+# EN NUMEROS ENTEROS SERIA 2 + 2 = 4, EN STRINGS SERIA "2" + "2" = "22" 
+
+print ("La clase de hoy estuvo de " + 10)
+
+
+
+
